d689e3ff7e0a94e8b3b06fdfc6cd6fe270c7ab45
Steve Li <lithafnium2000@gmail.com>
2024-03-08 16:56:06 -0500

    misc

---
+++ src/pages/misc/misc.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Bold, Link } from "@app/styles/styles";
+import { ContainerInner } from "./styles";
+const Misc = () => {
+  return (
+    <ContainerInner>
+      <p>some cool things i wanna share</p>
+    </ContainerInner>
+  );
+};
+
+export default Misc;
---
+++ src/pages/misc/styles.ts
@@ -0,0 +1,9 @@
+import { device, size } from "@app/shared/components/layout/layout";
+import { colors, fonts, grayscale } from "@app/styles/styles";
+import styled from "styled-components";
+
+export const ContainerInner = styled.div`
+  position: relative;
+  max-width: ${size.tablet};
+  width: 100%;
+`;
